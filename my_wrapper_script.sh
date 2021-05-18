df5fe6644d309cc6fbefc4fc0504e38306373783
Ju Ouyang <ju.ouayang@aicots.com>
2021-05-18 11:25:32 +0800

    Start nodejs with https

--- my_wrapper_script.sh
+++ my_wrapper_script.sh
@@ -9,7 +9,9 @@ if [ $status -ne 0 ]; then
 fi
 
 # Start the second process
-node /home/theia/src-gen/backend/main.js /home/project
+node /home/theia/src-gen/backend/main.js /home/project \
+  --ssl --cert /etc/nginx/certificates/cert.pem --certkey /etc/nginx/certificates/key.pem \
+  --hostname 127.0.0.1
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start theia: $status"
